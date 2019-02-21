4ced529c1b00c58f60d2c6cfa6f49d1fcb7968bf
Yuxiang Geng <gyxdtj120@hotmail.com>
2019-02-21 19:28:48 +0800

    uoupdate

---
+++ Assets/Scripts/Logic/Battle/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Battle
+{
+	/// <summary>
+	/// Everything in battle are Entity
+	/// </summary>
+	public class Entity
+	{
+
+	}
+}
---
+++ Assets/Scripts/Logic/Battle/TeamEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Battle
+{
+	/// <summary>
+	/// Entity which in team
+	/// </summary>
+	public class TeamEntity:Entity
+	{
+	}
+}
