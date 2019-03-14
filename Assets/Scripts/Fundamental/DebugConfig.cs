5656f20cab836c0d20e74b9056d98137b6102259
Yuxiang Geng <gyxdtj120@hotmail.com>
2019-03-14 19:22:25 +0800

    update

--- Assets/Scripts/Fundamental/DebugConfig.cs
+++ Assets/Scripts/Fundamental/DebugConfig.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Custom/Debug Config")]
-public class DebugConfig : ScriptableObject
+namespace Fundamental
 {
-	public bool Enable;
-	public bool[] Switch;
+	[CreateAssetMenu(menuName = "Custom/Debug Config")]
+	public class DebugConfig : ScriptableObject
+	{
+		public bool Enable;
+		public bool[] Switch;
+	}
 }
