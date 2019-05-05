d7237172f55cdf95f670dd21b4bf3b47350273c9
Yuxiang Geng <gyxdtj120@hotmail.com>
2019-05-05 19:06:02 +0800

    particle

---
+++ Assets/Scripts/Test/TestHotfix.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class TestHotfix : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public ParticleSystem ThisParticle;
+
+	
+	public void Start()
+	{
+		var main = ThisParticle.main;
+		main.startColor = new Color(0, 1, 0);
+	}
+
+	private void Awake()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
